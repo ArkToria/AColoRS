f435f6aad055c54a7119f9da44c1dd55b44103cc
khonoka <kagiyamahonoka@protonmail.com>
2022-01-30 16:38:13 +0800

    modify server debug message

--- crates/server/src/lib.rs
+++ crates/server/src/lib.rs
@@ -1,6 +1,9 @@
 use errors::{Error as ACrossError, Result};
 use log::debug;
 pub fn serve(interface: &str, interface_port: u16) -> Result<()> {
-    debug!("ACross Server is starting");
+    debug!(
+        "ACross Server is starting at {}:{}",
+        interface, interface_port
+    );
     Ok(())
 }

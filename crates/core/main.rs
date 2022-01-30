622e25395fd7bfac786f23601b5a787fad385a6f
khonoka <kagiyamahonoka@protonmail.com>
2022-01-30 16:36:46 +0800

    move function serve to a new file

--- crates/core/main.rs
+++ crates/core/main.rs
@@ -1,12 +1,10 @@
 use std::{error, process};
 
 use args::Args;
-use log::debug;
-use log::error;
-use utils::net::{tcp_get_available_port, tcp_port_is_available};
 
 mod app;
 mod args;
+mod serve;
 
 type Result<T> = ::std::result::Result<T, Box<dyn error::Error>>;
 fn main() {
@@ -22,49 +20,11 @@ fn try_main(args: Args) -> Result<()> {
     use args::Command::*;
 
     let matched = match args.command()? {
-        Serve => serve(&args),
+        Serve => serve::serve(&args),
     }?;
     if matched {
         process::exit(0)
     } else {
         process::exit(1)
-    }
-}
-fn serve(args: &Args) -> Result<bool> {
-    debug!("Serve with args: {:?}", args);
-    let matches = args
-        .matches()
-        .subcommand_matches("serve")
-        .unwrap_or_else(|| {
-            error!("No subcommand \"serve\".");
-            process::exit(1);
-        });
-    let interface = matches.value_of("interface").unwrap_or("[::1]");
-    let mut port: u16 = match matches.value_of("port").unwrap_or("19198").parse() {
-        Ok(x) => x,
-        Err(_) => {
-            error!("The port needs to be an integer");
-            process::exit(1);
-        }
-    };
-    if port != 19198 && !tcp_port_is_available(port) {
-        error!("The port is not available");
-        process::exit(1);
-    }
-
-    if !tcp_port_is_available(port) {
-        port = if let Some(p) = tcp_get_available_port(19198) {
-            p
-        } else {
-            error!("No port avaiable");
-            process::exit(1);
-        }
-    }
-    match server::serve(interface, port) {
-        Ok(()) => Ok(true),
-        Err(e) => {
-            error!("unravel error: {:?}", &e);
-            process::exit(1);
-        }
     }
 }
---
+++ crates/core/serve.rs
@@ -0,0 +1,47 @@
+use log::debug;
+use log::error;
+use utils::net::{tcp_get_available_port, tcp_port_is_available};
+
+use std::process;
+
+use crate::args::Args;
+use crate::Result;
+
+pub fn serve(args: &Args) -> Result<bool> {
+    debug!("Serve with args: {:?}", args);
+    let matches = args
+        .matches()
+        .subcommand_matches("serve")
+        .unwrap_or_else(|| {
+            error!("No subcommand \"serve\".");
+            process::exit(1);
+        });
+    let interface = matches.value_of("interface").unwrap_or("[::1]");
+    let mut port: u16 = match matches.value_of("port").unwrap_or("19198").parse() {
+        Ok(x) => x,
+        Err(_) => {
+            error!("The port needs to be an integer");
+            process::exit(1);
+        }
+    };
+    if port != 19198 && !tcp_port_is_available(port) {
+        error!("The port is not available");
+        process::exit(1);
+    }
+
+    if !tcp_port_is_available(port) {
+        port = if let Some(p) = tcp_get_available_port(19198) {
+            p
+        } else {
+            error!("No port avaiable");
+            process::exit(1);
+        }
+    }
+    match server::serve(interface, port) {
+        Ok(()) => Ok(true),
+        Err(e) => {
+            error!("unravel error: {:?}", &e);
+            process::exit(1);
+        }
+    }
+}

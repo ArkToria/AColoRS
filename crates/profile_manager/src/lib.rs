d41484b601720e2b6d48b86f8d70b8bdfe542d8c
khonoka <kagiyamahonoka@protonmail.com>
2022-02-03 21:09:45 +0800

    profile_manager: add pub use

--- crates/profile_manager/src/lib.rs
+++ crates/profile_manager/src/lib.rs
@@ -2,3 +2,14 @@ pub mod data_type;
 pub mod profile;
 pub mod table_member;
 pub mod tools;
+
+pub use data_type::group::GroupData;
+pub use data_type::node::NodeData;
+pub use data_type::runtimevalue::ValueData;
+
+pub use table_member::group::Group;
+pub use table_member::grouplist::GroupList;
+pub use table_member::node::Node;
+pub use table_member::runtime::RuntimeValue;
+
+pub use profile::Profile;
--- crates/profile_manager/src/profile.rs
+++ crates/profile_manager/src/profile.rs
@@ -1,6 +1,20 @@
+use std::rc::Rc;
+
+use rusqlite::Connection;
+
 use crate::table_member::{grouplist::GroupList, runtime::RuntimeValue};
 
+#[derive(Debug)]
 pub struct Profile {
     pub group_list: GroupList,
     pub runtime_value: RuntimeValue,
+}
+
+impl Profile {
+    pub fn new(connection: Rc<Connection>) -> Profile {
+        Profile {
+            group_list: GroupList::new(connection.clone()),
+            runtime_value: RuntimeValue::new(connection.clone()),
+        }
+    }
 }

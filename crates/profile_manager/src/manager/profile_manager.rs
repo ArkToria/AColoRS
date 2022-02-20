08e3f65d48b30f8dff71dab253e7330e157d068a
khonoka <kagiyamahonoka@protonmail.com>
2022-02-20 22:20:19 +0800

    profile_manager: add send_request! for ProfileTaskProducer

--- crates/profile_manager/src/manager/profile_manager.rs
+++ crates/profile_manager/src/manager/profile_manager.rs
@@ -11,6 +11,29 @@ use core_data::{GroupData, NodeData};
 use super::{consumer::create_consumer, reply::ProfileReply, request::ProfileRequest};
 use acolors_signal::AColorSignal;
 
+macro_rules! send_request {
+    ( $self:expr, $request:expr, ProfileReply::$reply:ident($($field:ident),*) ) => {{
+        let receiver = $self.send_request($request)?;
+
+        match receiver.await? {
+            ProfileReply::$reply($($field),*) => Ok($($field),*),
+            ProfileReply::Error(e) => Err(anyhow!("{}", e)),
+
+            _ => unreachable!(),
+        }
+    }};
+    ( $self:expr, $request:expr, ProfileReply::$reply:ident ) => {{
+        let receiver = $self.send_request($request)?;
+
+        match receiver.await? {
+            ProfileReply::$reply => Ok(()),
+            ProfileReply::Error(e) => Err(anyhow!("{}", e)),
+
+            _ => unreachable!(),
+        }
+    }};
+}
+
 #[derive(Debug)]
 pub struct ProfileTaskProducer {
     sender: mpsc::SyncSender<Request>,
@@ -37,151 +60,99 @@ impl ProfileTaskProducer {
     }
 
     pub async fn count_groups(&self) -> Result<usize> {
-        let content = ProfileRequest::CountGroups;
-        let receiver = self.send_request(content)?;
-
-        match receiver.await? {
-            ProfileReply::CountGroups(c) => Ok(c),
-            ProfileReply::Error(e) => Err(anyhow!("{}", e)),
-
-            _ => unreachable!(),
-        }
+        send_request!(
+            self,
+            ProfileRequest::CountGroups,
+            ProfileReply::CountGroups(value)
+        )
     }
 
     pub async fn list_all_groups(&self) -> Result<Vec<GroupData>> {
-        let content = ProfileRequest::ListAllGroups;
-        let receiver = self.send_request(content)?;
-
-        match receiver.await? {
-            ProfileReply::ListAllGroups(group_list) => Ok(group_list),
-            ProfileReply::Error(e) => Err(anyhow!("{}", e)),
-
-            _ => unreachable!(),
-        }
+        send_request!(
+            self,
+            ProfileRequest::ListAllGroups,
+            ProfileReply::ListAllGroups(value)
+        )
     }
 
     pub async fn count_nodes(&self, group_id: i32) -> Result<usize> {
-        let content = ProfileRequest::CountNodes(group_id);
-        let receiver = self.send_request(content)?;
-
-        match receiver.await? {
-            ProfileReply::CountNodes(c) => Ok(c),
-            ProfileReply::Error(e) => Err(anyhow!("{}", e)),
-
-            _ => unreachable!(),
-        }
+        send_request!(
+            self,
+            ProfileRequest::CountNodes(group_id),
+            ProfileReply::CountNodes(value)
+        )
     }
 
     pub async fn list_all_nodes(&self, group_id: i32) -> Result<Vec<NodeData>> {
-        let content = ProfileRequest::ListAllNodes(group_id);
-        let receiver = self.send_request(content)?;
-
-        match receiver.await? {
-            ProfileReply::ListAllNodes(node_list) => Ok(node_list),
-            ProfileReply::Error(e) => Err(anyhow!("{}", e)),
-
-            _ => unreachable!(),
-        }
+        send_request!(
+            self,
+            ProfileRequest::ListAllNodes(group_id),
+            ProfileReply::ListAllNodes(value)
+        )
     }
     pub async fn get_group_by_id(&self, group_id: i32) -> Result<GroupData> {
-        let content = ProfileRequest::GetGroupById(group_id);
-        let receiver = self.send_request(content)?;
-
-        match receiver.await? {
-            ProfileReply::GetGroupById(group_data) => Ok(group_data),
-            ProfileReply::Error(e) => Err(anyhow!("{}", e)),
-
-            _ => unreachable!(),
-        }
+        send_request!(
+            self,
+            ProfileRequest::GetGroupById(group_id),
+            ProfileReply::GetGroupById(value)
+        )
     }
     pub async fn get_node_by_id(&self, node_id: i32) -> Result<NodeData> {
-        let content = ProfileRequest::GetNodeById(node_id);
-        let receiver = self.send_request(content)?;
-
-        match receiver.await? {
-            ProfileReply::GetNodeById(node_data) => Ok(node_data),
-            ProfileReply::Error(e) => Err(anyhow!("{}", e)),
-
-            _ => unreachable!(),
-        }
+        send_request!(
+            self,
+            ProfileRequest::GetNodeById(node_id),
+            ProfileReply::GetNodeById(value)
+        )
     }
     pub async fn set_group_by_id(&self, group_id: i32, group_data: GroupData) -> Result<()> {
-        let content = ProfileRequest::SetGroupById(group_id, group_data);
-        let receiver = self.send_request(content)?;
-
-        match receiver.await? {
-            ProfileReply::SetGroupById => Ok(()),
-            ProfileReply::Error(e) => Err(anyhow!("{}", e)),
-
-            _ => unreachable!(),
-        }
+        send_request!(
+            self,
+            ProfileRequest::SetGroupById(group_id, group_data),
+            ProfileReply::SetGroupById
+        )
     }
     pub async fn set_node_by_id(&self, node_id: i32, node_data: NodeData) -> Result<()> {
-        let content = ProfileRequest::SetNodeById(node_id, node_data);
-        let receiver = self.send_request(content)?;
-
-        match receiver.await? {
-            ProfileReply::SetNodeById => Ok(()),
-            ProfileReply::Error(e) => Err(anyhow!("{}", e)),
-
-            _ => unreachable!(),
-        }
+        send_request!(
+            self,
+            ProfileRequest::SetNodeById(node_id, node_data),
+            ProfileReply::SetNodeById
+        )
     }
     pub async fn append_group(&self, group_data: GroupData) -> Result<()> {
-        let content = ProfileRequest::AppendGroup(group_data);
-        let receiver = self.send_request(content)?;
-
-        match receiver.await? {
-            ProfileReply::AppendGroup => Ok(()),
-            ProfileReply::Error(e) => Err(anyhow!("{}", e)),
-
-            _ => unreachable!(),
-        }
+        send_request!(
+            self,
+            ProfileRequest::AppendGroup(group_data),
+            ProfileReply::AppendGroup
+        )
     }
     pub async fn append_node(&self, node_id: i32, node_data: NodeData) -> Result<()> {
-        let content = ProfileRequest::AppendNode(node_id, node_data);
-        let receiver = self.send_request(content)?;
-
-        match receiver.await? {
-            ProfileReply::AppendNode => Ok(()),
-            ProfileReply::Error(e) => Err(anyhow!("{}", e)),
-
-            _ => unreachable!(),
-        }
+        send_request!(
+            self,
+            ProfileRequest::AppendNode(node_id, node_data),
+            ProfileReply::AppendNode
+        )
     }
     pub async fn remove_group_by_id(&self, group_id: i32) -> Result<()> {
-        let content = ProfileRequest::RemoveGroupById(group_id);
-        let receiver = self.send_request(content)?;
-
-        match receiver.await? {
-            ProfileReply::RemoveGroupById => Ok(()),
-            ProfileReply::Error(e) => Err(anyhow!("{}", e)),
-
-            _ => unreachable!(),
-        }
+        send_request!(
+            self,
+            ProfileRequest::RemoveGroupById(group_id),
+            ProfileReply::RemoveGroupById
+        )
     }
     pub async fn remove_node_by_id(&self, node_id: i32) -> Result<()> {
-        let content = ProfileRequest::RemoveNodeById(node_id);
-        let receiver = self.send_request(content)?;
-
-        match receiver.await? {
-            ProfileReply::RemoveNodeById => Ok(()),
-            ProfileReply::Error(e) => Err(anyhow!("{}", e)),
-
-            _ => unreachable!(),
-        }
+        send_request!(
+            self,
+            ProfileRequest::RemoveNodeById(node_id),
+            ProfileReply::RemoveNodeById
+        )
     }
 
     pub async fn update_group_by_id(&self, group_id: i32, nodes: Vec<NodeData>) -> Result<()> {
-        let content = ProfileRequest::UpdateGroup(group_id, nodes);
-        let receiver = self.send_request(content)?;
-
-        match receiver.await? {
-            ProfileReply::UpdateGroup => Ok(()),
-            ProfileReply::Error(e) => Err(anyhow!("{}", e)),
-
-            _ => unreachable!(),
-        }
+        send_request!(
+            self,
+            ProfileRequest::UpdateGroup(group_id, nodes),
+            ProfileReply::UpdateGroup
+        )
     }
 }
 

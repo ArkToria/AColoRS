f3a7ca140a1f6f3844c06f4a44b654d50522d9bb
asuna <null>
2022-03-14 00:06:24 +0800

    impl tcping function

--- crates/latency_test/src/lib.rs
+++ crates/latency_test/src/lib.rs
@@ -1,7 +1,10 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use std::time::{Duration, Instant};
+
+use anyhow::Result;
+use tokio::net::TcpStream;
+
+pub async fn tcping(address: &str, timeout: Duration) -> Result<Duration> {
+    let start = Instant::now();
+    tokio::time::timeout(timeout, TcpStream::connect(address)).await??;
+    Ok(Instant::now().duration_since(start))
 }
